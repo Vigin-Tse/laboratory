9606d2001284c7d459dc9403d8d4020758cfdbe9
xieweij <844065343@qq.com>
2020-12-28 15:36:53 +0800

    设计模式（概述）笔记-建造者模式

---
+++ basic/src/main/java/com/vg/basic/design/builder/ClientTest.java
@@ -0,0 +1,26 @@
+package com.vg.basic.design.builder;
+
+import com.vg.basic.design.builder.builder.ConcreteDecorator1;
+import com.vg.basic.design.builder.builder.Decorator;
+import com.vg.basic.design.builder.director.Director;
+import com.vg.basic.design.builder.product.Parlour;
+
+/**
+ * @description: 建造者模式测试
+ * @author: xieweij
+ * @create: 2020-12-28 15:21
+ **/
+public class ClientTest {
+
+    public static void main(String[] args){
+        //创建装修工1（建造者）
+        Decorator builder = new ConcreteDecorator1();
+
+        //创建指挥者，并制定装修工（建造者）
+        Director director = new Director(builder);
+
+        //创建产品
+        Parlour product = director.construct();
+        product.show();
+    }
+}
---
+++ basic/src/main/java/com/vg/basic/design/builder/product/Parlour.java
@@ -0,0 +1,39 @@
+package com.vg.basic.design.builder.product;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @description: 产品：客厅
+ * @author: xieweij
+ * @create: 2020-12-28 14:50
+ **/
+@Setter
+@Getter
+public class Parlour {
+
+    /**
+     * 电视
+     */
+    private String tv;
+
+    /**
+     * 沙发
+     */
+    private String sofa;
+
+    /**
+     * 墙
+     */
+    private String wall;
+
+    /**
+     * 显示产品特性
+     */
+    public void show(){
+        System.out.println("客厅电视:" + this.tv);
+        System.out.println("客厅沙发:" + this.sofa);
+        System.out.println("客厅墙:" + this.wall);
+
+    }
+}
