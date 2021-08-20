1580efe7c67b257e72005132b23f08553871dd1e
xieweij <844065343@qq.com>
2021-08-20 17:37:51 +0800

    synchronized相关源码研读

--- basic/src/main/java/com/vg/basic/lock/sync/SyncLock.java
+++ basic/src/main/java/com/vg/basic/lock/sync/SyncLock.java
@@ -12,23 +12,13 @@ public class SyncLock {
     }
 
     public static void main(String[] args){
-        System.out.println(10 % 2);
-        System.out.println(10 & (2 - 1));
 
-        System.out.println("------------------");
-        System.out.println(10 % 3);
-        System.out.println(10 & (3 - 1));
+        Number number = 11;
+        Integer integer = 12;
 
-        System.out.println("------------------");
-        System.out.println(10 % 4);
-        System.out.println(10 & (4 - 1));
+        number = integer;
 
-        System.out.println("------------------");
-        System.out.println(10 % 7);
-        System.out.println(10 & (7 - 1));
 
-        System.out.println("------------------");
-        System.out.println(10 % 8);
-        System.out.println(10 & (8 - 1));
+        System.out.println(number);
     }
 }
