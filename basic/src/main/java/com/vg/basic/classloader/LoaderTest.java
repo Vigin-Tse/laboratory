d2eae232f16aef58a93fedb4bdf825810ee0c22c
xieweij <844065343@qq.com>
2021-12-09 11:26:06 +0800

    类生命周期

---
+++ basic/src/main/java/com/vg/basic/classloader/LoaderTest.java
@@ -0,0 +1,38 @@
+package com.vg.basic.classloader;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+/**
+ * 指定class 进行加载e
+ */
+public class LoaderTest {
+    public static void main(String[] args) throws Exception {
+        URL classUrl = new URL("file:D:\\");//jvm 类放在位置
+
+        URLClassLoader parentLoader = new URLClassLoader(new URL[]{classUrl});
+
+        while (true) {
+            // 创建一个新的类加载器
+            URLClassLoader loader = new URLClassLoader(new URL[]{classUrl});
+
+            // 问题：静态块触发--对象初始化的时候，类加载器不会去触发
+            Class clazz = loader.loadClass("HelloService");
+            System.out.println("HelloService所使用的类加载器：" + clazz.getClassLoader());
+
+            Object newInstance = clazz.newInstance();
+            Object value = clazz.getMethod("test").invoke(newInstance);
+            System.out.println("调用getValue获得的返回值为：" + value);
+
+            Thread.sleep(3000L); // 1秒执行一次
+            System.out.println();
+
+            //  help gc  -verbose:class
+            newInstance = null;
+            loader = null;
+
+        }
+
+        // System.gc();
+    }
+}
---
+++ basic/src/main/java/com/vg/basic/classloader/Main.java
@@ -0,0 +1,11 @@
+package com.vg.basic.classloader;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+        System.in.read();
+    }
+}
