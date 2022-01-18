396d6fba0534d202ad3db92bb54a3cff65d5c4e8
xieweij <844065343@qq.com>
2022-01-18 15:35:11 +0800

    类加载机制

---
+++ basic/src/main/java/com/vg/basic/exvg/ExMain.java
@@ -0,0 +1,29 @@
+package com.vg.basic.exvg;
+
+import com.vg.ext.VgServiceImplInvoke;
+
+/**
+ * 使用线程上下文加载器打破双亲委派模型
+ * @author: xieweij
+ * @time: 2022/1/18 10:03
+ */
+public class ExMain {
+    public static void main(String[] args){
+
+//        ExV c;
+//
+//        VgServiceImpl vgService = new VgServiceImpl();
+//        vgService.setService(c = new ExV());
+//        vgService.doService();
+//
+//        System.out.println("ExV:" + c.getClass().getClassLoader());
+//        System.out.println("VgServiceImpl:" + vgService.getClass().getClassLoader());
+        System.out.println("com.vg.basic.exvg.ExMain：" + Thread.currentThread().getContextClassLoader());
+        VgServiceImplInvoke invoke = new VgServiceImplInvoke();
+        invoke.doService();
+
+
+
+//        System.out.println("com.vg.basic.exvg.VgServiceImplInvoke：" + invoke.getClass().getClassLoader());
+    }
+}
---
+++ basic/src/main/java/com/vg/basic/exvg/ExV.java
@@ -0,0 +1,16 @@
+package com.vg.basic.exvg;
+
+import com.vg.ext.VgService;
+
+/**
+ * @author: xieweij
+ * @time: 2022/1/18 9:54
+ */
+public class ExV implements VgService {
+
+
+    @Override
+    public void say() {
+        System.out.println("i am ExV");
+    }
+}
