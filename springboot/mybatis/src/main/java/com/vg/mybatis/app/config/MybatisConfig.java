128d72c40d53573e2d11c043bf71f05507af291d
xieweij <844065343@qq.com>
2022-03-14 14:12:50 +0800

    mybatis plus配置修改

--- springboot/mybatis/src/main/java/com/vg/mybatis/app/config/MybatisConfig.java
+++ springboot/mybatis/src/main/java/com/vg/mybatis/app/config/MybatisConfig.java
@@ -2,6 +2,7 @@ package com.vg.mybatis.app.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
@@ -38,8 +39,19 @@ public class MybatisConfig {
         mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
 
         // 新的分页插件,一缓和二缓遵循mybatis的规则,
-        // 需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+        // （（Mybatis Plus 3.4.0版本及其之后的版本）需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         return mybatisPlusInterceptor;
     }
+
+//    @Bean
+//    public ConfigurationCustomizer configurationCustomizer() {
+///*        return new ConfigurationCustomizer() {
+//            @Override
+//            public void customize(MybatisConfiguration configuration) {
+//                configuration.setUseDeprecatedExecutor(false);
+//            }
+//        };*/
+//        return configuration -> configuration.setUseDeprecatedExecutor(false);
+//    }
 }
