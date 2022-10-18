4cbe859b9bce1dd8ca0c25501c84d153321984c1
xieweij <844065343@qq.com>
2022-10-18 10:09:05 +0800

    修改redis配置

--- springboot/redis/src/main/java/com/vg/demo/redis/springdata/config/SpringDataRedisConfig.java
+++ springboot/redis/src/main/java/com/vg/demo/redis/springdata/config/SpringDataRedisConfig.java
@@ -1,5 +1,10 @@
 package com.vg.demo.redis.springdata.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -14,8 +19,10 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 @EnableCaching
@@ -52,21 +59,43 @@ public class SpringDataRedisConfig {
 
     /**
      * RedisTemplate
-     * <p>
      * redisTemplate.opsForValue();//操作字符串
      * redisTemplate.opsForHash();//操作hash
      * redisTemplate.opsForList();//操作list
      * redisTemplate.opsForSet();//操作set
      * redisTemplate.opsForZSet();//操作有序set
+     * <p>
+     * 序列化方式，spring-data-redis提供如下几种选择：
+     * GenericToStringSerializer: 可以将任何对象泛化为字符串并序列化
+     * Jackson2JsonRedisSerializer: 跟JacksonJsonRedisSerializer实际上是一样的
+     * JacksonJsonRedisSerializer: 序列化object对象为json字符串
+     * JdkSerializationRedisSerializer: 序列化java对象
+     * StringRedisSerializer: 简单的字符串序列化
      */
     @Bean
-    public RedisTemplate redisTemplate(LettuceConnectionFactory redisConnectionFactory) {
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate redisTemplate = new RedisTemplate();
 
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(
+                Object.class);
+        //json转对象类，不设置默认的会将json转成hashmap
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
+//        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());//设置redis key的序列化方式
-        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());// Object --> 序列化 --> 二进制流 --> redis-server存储
+        redisTemplate.setKeySerializer(new StringRedisSerializer()); //key 采用的String 的序列化方式
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());//hashde key 也采用String的序列化方式
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);//hash 的 value序列化方式采用jackson
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);//value 序列化方式采用jackson
+        redisTemplate.afterPropertiesSet();
+
         return redisTemplate;
+
     }
 
     /**
